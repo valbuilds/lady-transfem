b679062baa824a4b34293b39705305fd4652734b
Katie <katie@katiebuilds.website>
2023-05-02 01:36:57 -0500

    a

--- d.js
+++ d.js
@@ -1,5 +1,9 @@
 const { REST, Routes } = require('discord.js');
-const { clientId, guildId, token } = require('./config.json');
+const dotenv = require('dotenv');
+dotenv.config();
+const clientId = process.env.clientId;
+const guildId = process.env.guildId;
+const token = process.env.token;
 const fs = require('node:fs');
 const path = require('node:path');
 
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('node:fs');
 const path = require('node:path');
 const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
-const { token } = require(`./config.json`);
+const dotenv = require('dotenv');
+dotenv.config();
+const token = process.env.token;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
